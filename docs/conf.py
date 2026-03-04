6f9176ec27e4c2979cfb159785af16ce7e718c02
EttoreRocchi <ettoreroc@gmail.com>
2026-03-04 17:02:44 +0100

    [fix] typos [deploy-docs]

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,6 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "furo"
 html_logo = "phenocluster_logo.png"
 html_title = "PhenoCluster"
-html_static_path = ["_static"]
 
 html_theme_options = {
     "sidebar_hide_name": True,
